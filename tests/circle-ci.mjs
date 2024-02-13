ebd695b72d12d71430cff6b22222e3077f8d8c3c
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2024-02-13 09:47:07 +0100

    Fix CircleCi (#3)

--- tests/circle-ci.mjs
+++ tests/circle-ci.mjs
@@ -1,6 +1,9 @@
 import { MultiThreadz } from './../src/MultiThreadz.mjs'
 
-const mt = new MultiThreadz()
+const mt = new MultiThreadz( { 
+    'threads': 2,
+    'workerPath': './src/Workers/worker.mjs'
+} )
 const result = mt.health()
 
 if( result ) {
